1d6772d6cea88f61f520f4d76335a079e8be211c
letishabitu <146128673+letishabitu@users.noreply.github.com>
2023-11-03 05:31:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,77 +1,137 @@
-//Begin Class
-class Spaceship {
-    constructor(hull,firepower,accuracy){
-        this.hull = hull;
-        this.firepower = firepower;
-        this.accuracy = accuracy
-    }
-    attack(target){
-        if (Math.random() < this.accuracy){
-            console.log("succesful hit");
-            target.hull-=this.firepower
-        }
-        else{
-            console.log("hit missed");
-        }
-}
-}
-//End of Class
+const arrayOfWords = [["sport"], ["swim"], ["rain"]];
+//the length of the array of word. 
+let q = 3;
+const arrayOfMissingLetters = ["s _ o _  t", "s _ i _ ", "r _ _ n"]
 
-const element = document.getElementById('btn')
-element.addEventListener("click", function() {
-    document.getElementById("gamestatus").innerHTML = "<h1>Game started!</h1>";
-    const myShip = new Spaceship(20, 5, 0.7);
-    //console.log(myShip);
-    const enemyShips = [];
-    for (let i=0; i<6; i++){
-        enemyShips[i] = new Spaceship(Math.round(Math.random()*(6-3)+3), Math.round(Math.random()*(4-2)+2), Math.round((Math.random()*(0.8-0.6)+0.6)*10)/10)
-    }
-    //console.log(enemyShips)
+//the length of answer array which will be the innter array
+let p =2;
+const arrayOfAnswers = [["p", "r"], ["w", "m"],["a", "i"]]
+
+//to display list of words array with missing letters
+
+let number =0;
+function displayWord(){
+ 
+   if ( number < arrayOfMissingLetters.length){
+    getWords(number);
+    console.log(number);
+    number=number +1;
     
-    //Space battle begins
     
-    console.log('%c you are attacking an enemy ship','font-size: 20px');
-    document.getElementById("1attack").innerHTML="<h2>You are attacking an Enemy ship</h2>"
+   
+   }
 
-    while (myShip.hull>0 && enemyShips[0].hull >0){
-        myShip.attack(enemyShips[0])
-        if (enemyShips[0].hull>0){
-            enemyShips[0].attack(myShip)
-        }
-    }
+  }
 
-    if(enemyShips[0].hull<=0){
-        console.log('Enemy ship 1 is destroyed')
-        document.getElementById("1").innerHTML="<h2>First Enemy ship is destroyed,</h2>"
-        //confirm("would you like to attack more or retreat")
-        if(window.confirm("First Enemy ship is destroyed, would you like to attack more or retreat"))
-        {
-            for(k=1; k < enemyShips.length; k++)
-            {
-                while (myShip.hull>0 && enemyShips[k].hull >0){
-                    myShip.attack(enemyShips[k])
-                    if (enemyShips[k].hull>0 ){
-                        enemyShips[k].attack(myShip)
-                    }
-                    if(enemyShips[k].hull<=0)
-                    {
-                        console.log('enemy ship number ' + `${k+1}` + ' is destroyed')
-
-                        document.getElementById(`${k+1}`).innerHTML="<h2>Enemy ship " + `${k+1}` + " is destroyed</h2>"
-                    }
-                }
-            }
-        }
-        else
-        {
-            document.getElementById("gameend").innerHTML="<h2>GAME OVER</h2>"
-        }
-    } else if(myShip.hull <=0){
-        console.log('earth ship is destroyed. Game is over')
-        document.getElementById("1").innerHTML="<h2>My ship is destroyed Game over</h2>"
-    } else {
-        console.log('want to continue')
-        //confirm("would you like to attack more or retreat")
-    }
+//function to display the word, with some missing letters
+function getWords(number){
+  
+  const word1= document.getElementById("DisplayWord");
+  word1.innerHTML= arrayOfMissingLetters[number];
+  console.log(word1)
 }
-);
+const btn1= document.getElementById("word").addEventListener("click",displayWord);
+
+// const arrayOfUserResponse =[];
+//copying the user input, into an array 
+
+function returnArray(){
+     const retArra = document.getElementById("userInp");
+  retArra.innerHTML = userResponse;
+ }
+ let btn3 = document.getElementById("returnArray").addEventListener("click", returnArray);
+
+
+const userResponse =[];
+
+//function getting user response for the missing letters
+function returnLetter(){
+  //to check how many letters the user input for a given word
+        
+        const inp = document.getElementById("userInput").value;
+         console.log(inp);       
+         userResponse.push(inp);
+         alert(inp);
+        //  console.log(userResponse);       
+    
+}
+//function comparing user response with answers
+
+// const arrayOfCheck = [];
+
+let points=0;
+function validityCheck(){
+  number = number-1;
+   if ( number < arrayOfWords.length){
+    
+    checkValidity(number);
+    
+    console.log(number);   
+   }
+
+  }
+
+
+
+function checkValidity(number){
+    let k=0;
+    console.log(arrayOfWords[number]);  
+    
+    const correctWord = document.getElementById("correctword"); 
+    console.log(userResponse); 
+
+  for (let i=0; i<userResponse.length; i++){
+    if(arrayOfAnswers[number][i] == userResponse[i])
+      {     
+      k=k+1
+      }    
+
+    else{
+      k=k;             
+      }
+  const result = document.getElementById("result");
+  
+  }
+  
+  
+  
+   if (k == 2){
+    result.innerHTML = "correct"
+    points=points+10;     
+      console.log("correct");
+   }
+   else{
+    console.log("wrong");
+    points=points-10;
+    result.innerHTML = "Wrong";
+   }
+  
+   correctWord.innerHTML = arrayOfWords[number];
+   
+   console.log(arrayOfWords[number]);
+
+   console.log(points);
+   
+  //  number1 = number1+1;
+  //  console.log(number1);
+  }
+  // alert("click");
+  
+
+let btn = document.getElementById("validityCheck").addEventListener("click", validityCheck);
+
+
+function player1(){
+  const player1 = document.getElementById("user1");
+  player1.innerHTML = "player 1"; 
+    
+  }
+  let btn4 = document.getElementById("user1").addEventListener("click", player1);
+
+  function player2(){
+  const player1 = document.getElementById("user2");
+  player1.innerHTML = "player 2"; 
+    
+  }
+  let btn5 = document.getElementById("user2").addEventListener("click", player2);
+
